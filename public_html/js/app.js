4242db70816918407e5ee9076e7610b4b57547ca
Kaz van Wel <info@kiksaus.nl>
2021-11-17 16:03:39 +0100

    Example company

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,4 +1,4 @@
 (()=>{var __webpack_modules__={"./js/app.js":
 /*!*******************!*\
   !*** ./js/app.js ***!
-  \*******************/()=>{eval("// import a class example:\n// import {AppGallery} from './classes/gallery';\n// do stuff\n\n//# sourceURL=webpack://kikcms-website-assets/./js/app.js?")}},__webpack_exports__={};__webpack_modules__["./js/app.js"]()})();
+  \*******************/()=>{eval("// import {AppModule} from './classes/appModule';\nclass App {\n  init() {//this.module = (new AppModule(this)).init();\n  }\n\n}\n\nconst app = new App();\napp.init();\n\n//# sourceURL=webpack://kikcms-website-assets/./js/app.js?")}},__webpack_exports__={};__webpack_modules__["./js/app.js"]()})();
